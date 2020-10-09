188f72a2d148fb9ef63b401f2b8e8121e0f74c68
Rhys Adams <email.curly@gmail.com>
2020-10-09 13:18:14 +0100

    Added greetings, goodbyes and who subjects

--- Chat.java
+++ Chat.java
@@ -2,11 +2,20 @@
 class Chat{
 	
 	private final static String[] greetings = {"hi", "hello", "howdy", "greetings", "good morning", "good evening", "good day", "good afternoon"};
+	private final static String[] farewells = {"bye", "goodbye", "see you later", "good talking", "laters"};
+	
 	
 	public String respondOther(String query){
 		for(String greeting : greetings){
 			if(query.toLowerCase().contains(greeting)){
 				return "Hello there";
+			}
+		}
+		
+		for(String farewell : farewells){
+			if(query.toLowerCase().contains(farewell)){
+				System.out.println("See You again soon!");
+				System.exit(0);
 			}
 		}
 		
--- Question.java
+++ Question.java
@@ -1,40 +1,69 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 class Question{
 	public final static String[] questionSymbols = {"who", "which", "what", "why", "when", "where", "how", "?"};
 	
-	private String who(String question){
-		return "YES";
+	private final static Pattern whoPattern = Pattern.compile("who\\s+(is|are|am) (.*)", Pattern.MULTILINE);
+	
+	private static String who(String question){
+		Matcher matcher = whoPattern.matcher(question);
+		
+		
+		if(matcher.find()){
+			String subject = matcher.group(2);
+			
+			return subject;
+		}
+		
+		return "you";
 	}
 	
-	private String which(String question){
-		return "YES";
+	private static String which(String question){
+		return "the first one";
 	}
 	
-	private String what(String question){
-		return "YES";
+	private static String what(String question){
+		return "whatever you want it to be";
 	}
 	
-	private String why(String question){
-		return "YES";
+	private static String why(String question){
+		return "because it is";
 	}
 	
-	private String when(String question){
-		return "YES";
+	private static String when(String question){
+		return "at some point";
 	}
 	
-	private String where(String question){
-		return "YES";
+	private static String where(String question){
+		return "probably where you least expect it";
 	}
 	
-	private String how(String question){
-		return "YES";
+	private static String how(String question){
+		return "i don't even know myself";
 	}
 		
 	
 	public static String answer(String question){
 		String firstWord = question.split(" ")[0];
 		
-		System.out.println(":"+firstWord);
+		switch(firstWord.toLowerCase()){
+			case "who":
+				return who(question);
+			case "which":
+				return which(question);
+			case "what":
+				return what(question);
+			case "why":
+				return why(question);
+			case "when":
+				return when(question);
+			case "where":
+				return where(question);
+			case "how":
+				return how(question);
+		}
 		
-		return "YES";
+		return "Yessss";
 	}
 }
